aea599047ce3ac11913dfd40b3f4590c9f3b466e
Rob Court <robbie.court@gmail.com>
2018-04-04 17:28:40 +0100

    Update loadScript.R

--- loadScript.R
+++ loadScript.R
@@ -1,25 +1,9 @@
 #!/usr/bin/env Rscript
 
-.libPaths()
-.libPaths( c( "/usr/local/lib/R/site-library") )
-.libPaths( c( .libPaths(), "/usr/lib/R/library") )
-.libPaths( c( .libPaths(), "/usr/lib/R/site-library") )
-.libPaths( c( .libPaths(), "/usr/local/lib/opencpu/site-library") )
-.libPaths( c( .libPaths(), "/usr/lib/opencpu/library") )
-.libPaths( c( .libPaths(), "/usr/lib/opencpu/site-library") )
-.libPaths()
 options(flycircuit.datadir="/data")
 options(rgl.useNULL=TRUE)
 if(!require("devtools")) install.packages("devtools")
-# this will install all the regular nat packages
-devtools::source_gist("39a1182f726989db7e03", filename="install_all_nat.R")
-# elmr will bring in all the other packages we need
-devtools::install_github("jefferis/elmr", dependencies=TRUE)
-message("Setting up flycircuit dataset including pre-computed NBLAST scores")
-devtools::source_gist("bbaf5d53353b3944c090", filename = "FlyCircuitStartupNat.R")
+install_github("aoles/shinyURL")
+install.packages(c("shiny","ggplot2", "downloader", "rglwidget"))
+devtools::source_gist("fdd1e5b6e009ff49e66be466a104fd92", filename = "install_flyconnectome_all.R")
 
-message("Downloading flycircuit neurons for de novo NBLAST - this could take 15m to overnight")
-library(flycircuit)
-dps<-read.neuronlistfh("http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/dpscanon.rds",
-    localdir=getOption('flycircuit.datadir'))
-remotesync(dps,download.missing = TRUE)
