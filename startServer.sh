56918a403317b36e46ccacacc8162e89f79417b1
Rob Court <rcourt@ed.ac.uk>
2022-02-08 14:58:43 +0000

    reducing startup time

--- startServer.sh
+++ startServer.sh
@@ -6,19 +6,9 @@ if [ "$FASTBOOT" != "false" ]; then
   sed -i "s|install_flyconnectome_all.R|install_flyconnectome_packages.R|" /loadScript.R
 fi
 
-Rscript /loadScript.R
-
-rm -r /usr/local/lib/R/site-library/00LOCK-*
-
-chmod -R 777 /usr/local/lib/R/site-library
-chmod -R 777 /usr/lib/R/site-library
-
-rm -r /usr/local/lib/R/site-library/00LOCK-*
-
 while true; do for file in /var/log/shiny-server/*.log; do if [ -e $file ]; then cat $file; fi; done; sleep 40; done &
 
 Rscript /loadScript.R
-
 
 cd /tmp/ && sleep 1m && wget http://localhost:3838/NBLAST_on-the-fly/ &
 
