55dc22021f4b01e3c49f1edc209dda060b8138ac
Rob Court <rcourt@ed.ac.uk>
2022-02-09 07:11:17 +0000

    forcing permissions

--- startServer.sh
+++ startServer.sh
@@ -8,7 +8,16 @@ fi
 
 while true; do for file in /var/log/shiny-server/*.log; do if [ -e $file ]; then cat $file; fi; done; sleep 40; done &
 
-Rscript /loadScript.R
+
+chmod -R 777 /usr/local/lib/R/site-library
+chmod -R 777 /usr/lib/R/site-library
+chmod -R 777 /home/shiny/.local
+rm -r /usr/local/lib/R/site-library/00LOCK-*
+
+sudo -u shiny Rscript /loadScript.R
+
+rm -r /usr/local/lib/R/site-library/00LOCK-*
+chmod -R 777 /home/shiny/.local
 
 cd /tmp/ && sleep 1m && wget http://localhost:3838/NBLAST_on-the-fly/ &
 
